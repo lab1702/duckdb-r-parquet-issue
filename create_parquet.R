9ea1cff8b5623ad2a545f26c834ae4a58011b7c1
Lars Bernhardsson <lab1702@gmail.com>
2026-03-31 19:46:37 -0400

    Add files via upload

--- create_parquet.R
+++ create_parquet.R
@@ -3,7 +3,7 @@ library(arrow)
 n <- 100
 
 df <- data.frame(test_value = sample(1:1000, n, replace = TRUE))
-df$test_date <- seq_along(df$test_value) + Sys.Date() - 100
+df$test_date <- seq_along(df$test_value) + Sys.Date() - n
 
 write_parquet(df, "test_data.parquet")
 cat("Wrote", nrow(df), "rows to test_data.parquet\n")
